01b278f4e75700cadfd3616f2b637ed418e1c261
Rbne <renehgren@gmail.com>
2022-12-12 13:21:42 +0200

    Menüü arendus enda branchi peal

--- Surmatants.py
+++ Surmatants.py
@@ -1,9 +1,10 @@
 import pygame
 from player import Player
-import Button as B
+from Button import *
 import os
 import sys
 from pygame import mixer
+import time
 
 print(Player)
 
@@ -21,47 +22,47 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 player1 = Player(270, 400, SCREEN_WIDTH)
 player2 = Player(810, 400, SCREEN_WIDTH)
 
-bg_image = pygame.image.load(os.path.join("Assets","test.jpg")).convert_alpha()
+bg_image = pygame.image.load(os.path.join("Assets","areen", "test.jpg")).convert_alpha()
 bg_menu = pygame.image.load(os.path.join("Assets","white_rain.jpg")).convert_alpha()
+
 #Making the game run uniformly on 60FPS
 clock = pygame.time.Clock()
 FPS = 60
 
 #Background image function
-# def bg():
-#     screen.blit(bg_image, (0,0))
 
 click_sound = mixer.Sound((os.path.join("Assets","sounds", "click.mp3")))
 click_sound.set_volume(0.5)
 thunder_sound = mixer.Sound((os.path.join("Assets","sounds", "thunder.mp3")))
 thunder_sound.set_volume(0.6)
+sound = mixer.Sound((os.path.join("Assets","sounds", "sound.wav")))
+sound.set_volume(0.6)
 
 def main_menu():
+    global current_time
+    current_time = time.time()
     # Caption for the Menu page
     pygame.display.set_caption("Menu")
     mixer.music.load((os.path.join("Assets","sounds", "bg_storm.mp3")))
     mixer.music.play(-1)
     mixer.music.set_volume(0.5)
 
-    # Loading of sound and setting volume
-
 
     run = True 
-    # Images for the start and exit buttons (Test images in this case)
-    start_img = pygame.image.load(os.path.join("Assets", "test_button.png")).convert_alpha()
-    exit_img = pygame.image.load(os.path.join("Assets","test_button.png")).convert_alpha() 
 
     # Making use of the button class in Button.py and putting buttons to middle with methods middle_pos
-    start_button = B.Button(SCREEN_WIDTH/2, 250, "", 0.5, "Alusta", False)
+    start_button = Button(SCREEN_WIDTH/2, 250, 0.5, "Alusta")
+    exit_button = Button(SCREEN_WIDTH/2, 450, 0.5, "Sulge")
+    level_button = Button(SCREEN_WIDTH/2, 350, 0.5, "Areen")
+    button = Button(1,1,1,"",)
+    button.saladus()
 
-    exit_button = B.Button(SCREEN_WIDTH/2, 450, "", 0.5, "Sulge", False)
-    level_button = B.Button(SCREEN_WIDTH/2, 350, start_img, 0.5, "Areen", False)
+
     
     # Title font stuff
     title = title_font.render("Surmatants", 1, (255,255,255))
     title_rect  = title.get_rect(center=(SCREEN_WIDTH/2, 100))
-
-    
+    vajutuste_piiraja = 1
     while run:
         
         #screen.fill((0,0,0))
@@ -72,21 +73,24 @@ def main_menu():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-        if start_button.draw(screen):
+        if start_button.draw(screen) and time.time() - current_time > 0.1:
             thunder_sound.play()
             mixer.music.fadeout(500)
             print("START")
             main()
             
-        elif exit_button.draw(screen):
+        elif exit_button.draw(screen) and time.time() - current_time > 0.1:
             run = False
             print("EXIT")
             pygame.quit()
             break
-        elif level_button.draw(screen):
+        elif level_button.draw(screen) and time.time() - current_time > 0.1:
+            current_time = time.time()
             click_sound.play()
             areen()
-
+        elif button.draw(screen) and time.time() - current_time > 0.1 and vajutuste_piiraja == 1:
+            sound.play()
+            vajutuste_piiraja = 0
        # Checks for keys pressed and makes use of 'Q' for quitting the window
         elif keys_pressed[pygame.K_q]:
             run = False
@@ -96,42 +100,81 @@ def main_menu():
     sys.exit()
 
 def areen():
-    global bg_image
+    global bg_image, current_time
     pygame.display.set_caption("Areen")
     run = True
     areen_sound = mixer.Sound((os.path.join("Assets","sounds", "areen_select2.wav")))
     areen_sound.set_volume(0.2)
-    #Areenid images
-    galaktika = pygame.image.load(os.path.join("Assets", "galaktika_resize.jpg")).convert_alpha()
-    estcube = pygame.image.load(os.path.join("Assets", "estcube2.jpg")).convert_alpha()
+    praegune_kuu = time.localtime()[1]
 
-    galaktika_button = B.Button(100, SCREEN_HEIGHT/2, galaktika, 0.25, "Galaktika", True )
-    galaktika_button.arena_button()
-    estcube_button = B.Button(500, SCREEN_HEIGHT/2, estcube, 0.25, "Galaktika", True )
-    estcube_button.arena_button()
-    tagasi_button = B.Button(SCREEN_WIDTH/2, 540, "", 0.5, "Tagasi", False)
+    #Areenid images
+    galaktika = pygame.image.load(os.path.join("Assets","areen", "galaktika.jpg")).convert_alpha()
+    physicum = pygame.image.load(os.path.join("Assets","areen", "physicum.jpg")).convert_alpha()
+    taj_mahal = pygame.image.load(os.path.join("Assets","areen", "taj_mahal.jpg")).convert_alpha()
+    toomkirik = pygame.image.load(os.path.join("Assets","areen", "toomkirik.jpg")).convert_alpha()
+    versailles = pygame.image.load(os.path.join("Assets","areen", "versailles.jpg")).convert_alpha()
+
+    if praegune_kuu == 12:
+        raekoda = pygame.image.load(os.path.join("Assets","areen", "raekoda_talv.jpg")).convert_alpha()
+    else:
+        raekoda = pygame.image.load(os.path.join("Assets","areen", "raekoda.jpg")).convert_alpha()
+    
+    esimene_rida = 54
+    teine_rida = 324
+    veerg_1 = 67
+    veerg_2 = 404
+    veerg_3 = 741
+
+    galaktika_button = arena_button(veerg_1, esimene_rida, galaktika, 0.25, "galaktika")
+    physicum_button = arena_button(veerg_2, esimene_rida, physicum, 0.25, "physicum")
+    tajmahal_button = arena_button(veerg_3, esimene_rida, taj_mahal, 0.25, "taj mahal")
+    toomkirik_button = arena_button(veerg_1, teine_rida, toomkirik, 0.25, "toomkirik")
+    versailles_button = arena_button(veerg_2, teine_rida, versailles, 0.25, "versailles")
+    raekoda_button = arena_button(veerg_3, teine_rida, raekoda, 0.25, "raekoda")
+
+    tagasi_button = Button(SCREEN_WIDTH/2, SCREEN_HEIGHT - 50, 0.5, "Tagasi")
 
     while run:
         screen.blit(bg_menu,(0,0))
+        keys_pressed = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
                 sys.exit()
-        if galaktika_button.draw(screen):
+
+        if galaktika_button.draw(screen) and time.time() - current_time > 0.1:
             areen_sound.play()
             bg_image = galaktika
             main_menu()
-            run = False
-        elif estcube_button.draw(screen):
-            areen_sound.play
-            bg_image = estcube
+        elif physicum_button.draw(screen) and time.time() - current_time > 0.1:
+            areen_sound.play()
+            bg_image = physicum
             main_menu()
-            run = False
-        elif tagasi_button.draw(screen):
+        elif tajmahal_button.draw(screen) and time.time() - current_time > 0.1:
+            areen_sound.play()
+            bg_image = taj_mahal
+            main_menu()
+        elif toomkirik_button.draw(screen) and time.time() - current_time > 0.1:
+            areen_sound.play()
+            bg_image = toomkirik
+            main_menu()
+        elif versailles_button.draw(screen) and time.time() - current_time > 0.1:
+            areen_sound.play()
+            bg_image = versailles
+            main_menu()
+        elif raekoda_button.draw(screen) and time.time() - current_time > 0.1:
+            areen_sound.play()
+            bg_image = raekoda
+            main_menu()
+
+        elif tagasi_button.draw(screen) and time.time() - current_time > 0.1:
             click_sound.play()
             main_menu()
+
+        elif keys_pressed[pygame.K_q]:
             run = False
+            break
         pygame.display.update()
 
 
